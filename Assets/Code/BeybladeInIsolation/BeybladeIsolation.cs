6832a7d4e3124bb12ea386cf6b6d54f959da4b0a
HiryuJupiter <xooase@gmail.com>
2021-05-11 12:57:29 +1000

    end of Tuesday morning class

---
+++ Assets/Code/BeybladeInIsolation/BeybladeIsolation.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody2D))]
+public class BeybladeIsolation : MonoBehaviour
+{
+    Rigidbody2D rb;
+
+
+
+    private void Awake()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        
+    }
+
+
+    void Shoot ()
+    {
+
+    }
+}
---
+++ Assets/Code/BeybladeInIsolation/BeybladeMotor.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using UnityEngine;
+
+public class BeybladeMotor : MonoBehaviour
+{
+
+    void Start()
+    {
+
+    }
+
+    void Update()
+    {
+
+    }
+}
