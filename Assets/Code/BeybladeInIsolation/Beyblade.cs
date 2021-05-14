7db256562db7cd7d74f0519c33da55b92c29faa4
HiryuJupiter <xooase@gmail.com>
2021-05-14 14:40:03 +1000

    Basic ball moving working

--- Assets/Code/BeybladeInIsolation/Beyblade.cs
+++ Assets/Code/BeybladeInIsolation/Beyblade.cs
@@ -3,34 +3,126 @@ using System.Collections.Generic;
 using UnityEngine;
 
 [RequireComponent(typeof(Rigidbody2D))]
-[RequireComponent(typeof(BeybladeGraphics))]
-[RequireComponent(typeof(BeybladePhysics))]
 public class Beyblade : MonoBehaviour
 {
+    #region MonoBehavior
+    [SerializeField] Transform spriteRoot;
+    [SerializeField] TextMesh text;
 
-    BeybladePhysics physics;
-    BeybladeGraphics graphics;
+    float moveSpeed = 25f;
+    float rotationSpeed = 2f;
+    Rigidbody2D rb;
 
+    //Status
+    int score;
+    bool isMoving;
+    bool doDamp;
+    float curMoveDur;
+    float timeBeforeDampingStart = 0.1f;
+    float dampAmount = .99f;
+
+    public Vector2 Velocity => rb.velocity;
 
     private void Awake()
     {
-        physics = GetComponent<BeybladePhysics>();
-        graphics = GetComponent<BeybladeGraphics>();
-    }
-
-    void Start()
-    {
-        
+        rb = GetComponent<Rigidbody2D>();
     }
 
     void Update()
     {
-        
+        if (isMoving)
+        {
+            MovingUpdate();
+        }
+        else
+        {
+            RotateArrow();
+            ShootBallWhenPressed();
+        }
     }
+    #endregion
 
-
-    void Shoot ()
+    #region Collision
+    void OnCollisionEnter2D(Collision2D collision)
     {
-
+        HitsGameObject(collision.gameObject);
     }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        HitsGameObject(collision.gameObject);
+    }
+
+    void HitsGameObject (GameObject go)
+    {
+        if (go.tag == "Dot")
+        {
+            IncrementScore();
+            Destroy(go);
+            DotSpawner.Spawn();
+        }
+    }
+    #endregion
+
+    #region Rotating and waiting to shoot
+    void RotateArrow()
+    {
+        spriteRoot.Rotate(new Vector3(0f, 0f, rotationSpeed));
+    }
+
+    void ShootBallWhenPressed()
+    {
+        if (Input.GetMouseButtonDown(0) || Input.GetKeyDown(KeyCode.Space) || Input.GetKeyDown(KeyCode.Return)) //Shoot
+        {
+            BeginMoving();
+        }
+    }
+
+    #endregion
+
+    #region Moving
+    void BeginMoving ()
+    {
+        rb.velocity = ArrowUpDir() * moveSpeed;
+        doDamp = false;
+        isMoving = true;
+    }
+
+    void MovingUpdate()
+    {
+        if (doDamp)
+        {
+            rb.velocity *= dampAmount;
+            if (rb.velocity.sqrMagnitude < 1f)
+            {
+                isMoving = false;
+            }
+        }
+        else
+        {
+            curMoveDur += Time.deltaTime;
+            if (curMoveDur > timeBeforeDampingStart)
+                doDamp = true;
+        }
+
+        MakeArrowFaceMovingDirection();
+    }
+
+    void MakeArrowFaceMovingDirection()
+    {
+        spriteRoot.rotation = Quaternion.LookRotation(Vector3.forward, rb.velocity);
+    }
+    #endregion
+
+    #region Score
+    void IncrementScore ()
+    {
+        score++;
+        text.text = score.ToString();
+    }
+    #endregion
+
+    #region Minor
+    Vector3 ArrowUpDir() => spriteRoot.transform.up;
+    #endregion
 }
---
+++ Assets/Code/BeybladeInIsolation/DotSpawner.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using UnityEngine;
+
+public class DotSpawner : MonoBehaviour
+{
+    public static DotSpawner Instance;
+
+    [SerializeField] GameObject dot;
+    [SerializeField] float spawnRange_x;
+    [SerializeField] float spawnRange_y;
+
+    private void Awake()
+    {
+        Instance = this;
+        SpawnDot();
+    }
+
+    void SpawnDot()
+    {
+        Instantiate(dot, GetSpawnPos(), Quaternion.identity);
+    }
+
+    public static void Spawn() => Instance.SpawnDot();
+
+
+    Vector2 GetSpawnPos ()
+    {
+        return new Vector2(Random.Range(-spawnRange_x, spawnRange_x), Random.Range(-spawnRange_y, spawnRange_y));
+    }
+}
